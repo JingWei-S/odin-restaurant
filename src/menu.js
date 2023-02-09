673726225a94bc26af9840131b0dd369fca959bf
JingWei-S <weijing.524@gmail.com>
2023-02-09 21:59:02 +1100

    add the name and adjust ui designs

--- src/menu.js
+++ src/menu.js
@@ -5,6 +5,8 @@ const createMenu = () => {
 
     const food_photo_src = ['./images/brunch 1.png', './images/brunch 2.png', './images/brunch 3.png',
     './images/brunch 4.png', './images/brunch 5.png', './images/brunch 6.png'];
+    const food_names = ["The Devil's Guacamole Grin", "Hell's Flaky Breakfast", "Monster's Frozen Feast",
+    "Blood-Thirsty Burrito", "The Brioche of Despair", "Devil's Burning Craving"];
 
     for (let i = 0; i < 6; i++) {
         const food_div = document.createElement('div');
@@ -12,7 +14,7 @@ const createMenu = () => {
         const food_img = document.createElement('img');
         food_img.src = food_photo_src[i];
         const food_name = document.createElement('p');
-        food_name.textContent = 'This is brunch';
+        food_name.textContent = food_names[i];
         food_div.appendChild(food_img);
         food_div.appendChild(food_name);
         menu.appendChild(food_div);
